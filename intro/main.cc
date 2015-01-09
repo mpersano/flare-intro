02b71dde0ee36c98ac3fd363955001e912d52210
fzort <fzort@tycho.luna>
2015-01-09 15:02:38 +0000

    Added ogg_player

--- intro/main.cc
+++ intro/main.cc
@@ -3,7 +3,12 @@
 #include <GL/glew.h>
 
 #include <ggl/window.h>
+#include <ggl/panic.h>
 
+#include <AL/alc.h>
+#include <AL/al.h>
+
+#include "ogg_player.h"
 #include "fx.h"
 #include "tube.h"
 #include "util.h"
@@ -22,10 +27,19 @@ class intro_window : public ggl::window
 {
 public:
 	intro_window();
+	~intro_window();
 
+	void start_music();
 	void draw();
 
 private:
+	void init_openal();
+	void release_openal();
+
+	ALCdevice *al_device_;
+	ALCcontext *al_context_;
+
+	std::unique_ptr<ogg_player> player_;
 	std::unique_ptr<fx> fx_;
 	float start_t_;
 };
@@ -34,11 +48,52 @@ intro_window::intro_window()
 : ggl::window(512, 512)
 , fx_(new tube(width_, height_))
 , start_t_(now())
-{ }
+{
+	init_openal();
+}
+
+intro_window::~intro_window()
+{
+	release_openal();
+}
+
+void
+intro_window::init_openal()
+{
+	if (!(al_device_ = alcOpenDevice(nullptr)))
+		panic("alcOpenDevice failed");
+
+	if (!(al_context_ = alcCreateContext(al_device_, nullptr)))
+		panic("alcCreateContext failed");
+
+	alcMakeContextCurrent(al_context_);
+	alGetError();
+}
+
+void
+intro_window::release_openal()
+{
+	alcMakeContextCurrent(nullptr);
+	alcDestroyContext(al_context_);
+	alcCloseDevice(al_device_);
+}
+
+void
+intro_window::start_music()
+{
+	player_.reset(new ogg_player);
+
+	player_->open("music.ogg");
+
+	player_->set_gain(1.);
+	player_->start();
+}
 
 void
 intro_window::draw()
 {
+	player_->update();
+
 	glViewport(0, 0, width_, height_);
 	glClearColor(0, 0, 0, 0);
 	glClear(GL_COLOR_BUFFER_BIT);
@@ -51,5 +106,8 @@ intro_window::draw()
 int
 main()
 {
-	intro_window().run();
+	intro_window intro;
+
+	intro.start_music();
+	intro.run();
 }
---
+++ intro/ogg_player.cc
@@ -0,0 +1,223 @@
+#include <cassert>
+#include <cstring>
+
+#include <ggl/panic.h>
+
+#include "ogg_player.h"
+
+ogg_player::ogg_player()
+: playing(false)
+{
+	alGenSources(1, &source);
+	set_gain(1);
+}
+
+ogg_player::~ogg_player()
+{
+	close();
+	alDeleteSources(1, &source);
+}
+
+void
+ogg_player::open(const std::string& path)
+{
+	if (!(ogg_file = fopen(path.c_str(), "rb")))
+		panic("failed to open `%s'", path.c_str());
+
+	if (ov_open(ogg_file, &ogg_stream, NULL, 0) < 0)
+		panic("failed to open ogg stream");
+
+	const vorbis_info *info = ov_info(&ogg_stream, -1);
+
+	switch (info->channels) {
+		case 1:
+			format = AL_FORMAT_MONO16;
+			break;
+
+		case 2:
+			format = AL_FORMAT_STEREO16;
+			break;
+
+		default:
+			panic("invalid # of channels");
+	}
+
+	rate = info->rate;
+
+	num_samples = ov_pcm_total(&ogg_stream, -1);
+
+	printf("channels=%d rate=%d samples=%d\n", info->channels, info->rate, num_samples);
+}
+
+void
+ogg_player::close()
+{
+	stop();
+
+	ov_clear(&ogg_stream);
+}
+
+void
+ogg_player::set_gain(float g)
+{
+	gain = g;
+	alSourcef(source, AL_GAIN, gain);
+}
+
+void
+ogg_player::fade_out(int ttl)
+{
+	fading_out = true;
+	fade_out_ttl = ttl;
+	fade_out_tics = 0;
+}
+
+void
+ogg_player::start()
+{
+	if (playing)
+		return;
+
+	for (int i = 0; i < NUM_BUFFERS; i++) {
+		buffer& b = buffers[i];
+
+		if (b.load(&ogg_stream) > 0)
+			b.queue(source, format, rate);
+	}
+
+	alSourcePlay(source);
+
+	playing = true;
+	fading_out = false;
+}
+
+void
+ogg_player::stop()
+{
+	if (!playing)
+		return;
+
+	alSourceStop(source);
+
+	ALint num_processed;
+	while (alGetSourcei(source, AL_BUFFERS_PROCESSED, &num_processed), num_processed > 0) {
+		ALuint id;
+		alSourceUnqueueBuffers(source, 1, &id);
+	}
+
+	ov_raw_seek(&ogg_stream, 0);
+
+	playing = false;
+}
+
+void
+ogg_player::update()
+{
+	if (!playing)
+		return;
+
+	ALint state;
+	alGetSourcei(source, AL_SOURCE_STATE, &state);
+
+	ALint num_processed;
+	while (alGetSourcei(source, AL_BUFFERS_PROCESSED, &num_processed), num_processed > 0) {
+		ALuint id;
+		alSourceUnqueueBuffers(source, 1, &id);
+
+		buffer *p = get_buffer(id);
+		if (p->load(&ogg_stream) > 0)
+			p->queue(source, format, rate);
+	}
+
+	if (state == AL_PLAYING && fading_out) {
+		if (++fade_out_tics >= fade_out_ttl) {
+			stop();
+			fading_out = false;
+		} else {
+			const float g = gain*(1. - static_cast<float>(fade_out_tics)/fade_out_ttl);
+			alSourcef(source, AL_GAIN, g);
+		}
+	}
+
+	if (state != AL_PLAYING) {
+		ALint queued;
+
+		alGetSourcei(source, AL_BUFFERS_QUEUED, &queued);
+
+		if (queued == 0) {
+			// we're done
+			playing = false;
+		} else {
+			// restart
+			alSourcePlay(source);
+		}
+	}
+}
+
+int
+ogg_player::get_num_buffer_samples() const
+{
+	switch (format) {
+		case AL_FORMAT_MONO16:
+			return buffer::BUFFER_SIZE/sizeof(int16_t);
+
+		case AL_FORMAT_STEREO16:
+			return buffer::BUFFER_SIZE/(2*sizeof(int16_t));
+
+		default:
+			assert(0);
+			return 0;
+	}
+}
+
+ogg_player::buffer *
+ogg_player::get_buffer(ALuint id)
+{
+	for (int i = 0; i < NUM_BUFFERS; i++) {
+		buffer *p = &buffers[i];
+		if (p->id == id)
+			return p;
+	}
+
+	panic("eh? invalid buffer %d?", id);
+	return 0;
+}
+
+ogg_player::buffer::buffer()
+{
+	alGenBuffers(1, &id);
+}
+
+ogg_player::buffer::~buffer()
+{
+	alDeleteBuffers(1, &id);
+}
+
+long
+ogg_player::buffer::load(OggVorbis_File *stream)
+{
+	size = 0;
+
+	while (size < BUFFER_SIZE) {
+		int section;
+		long r = ov_read(stream, data + size, BUFFER_SIZE - size, 0, 2, 1, &section);
+
+		if (r < 0)
+			panic("ov_read failed");
+		else if (r == 0)
+			break;
+
+		size += r;
+	}
+
+	return size;
+}
+
+void
+ogg_player::buffer::queue(ALuint source, ALenum format, int rate)
+{
+	if (size > 0) {
+		alBufferData(id, format, data, size, rate);
+		alSourceQueueBuffers(source, 1, &id);
+	}
+}
